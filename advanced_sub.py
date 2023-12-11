735153d1b29fa60698c5116259527e656f89a129
irina <92105302+nosova-iv@users.noreply.github.com>
2023-12-11 23:16:58 +0300

    Update advanced_sub.py

--- advanced_sub.py
+++ advanced_sub.py
@@ -8,34 +8,34 @@
 # aZc   ---> a!Z!c
 # aZZc  ---> a!Z!!Z!c
 # aBaCa ---> a!B!a!C!a
-REGEXP_1 = ''   # регулярное выражение
-REGEXP_1_REPL = '' # выражение для строки замены
+REGEXP_1 = r'[ABCZ]'   # регулярное выражение
+REGEXP_1_REPL = r'!\g<0>!' # выражение для строки замены
 
 # abc    ---> abc
 # abbc   ---> abc
 # azzzc  ---> azc
 # arrrrc ---> arc
 # xxxxxx ---> x
-REGEXP_2 = '' 
-REGEXP_2_REPL = ''
+REGEXP_2 = r'(.)\1+' 
+REGEXP_2_REPL = r'\1'
 
 # this is text         ---> this is text
 # this is is text      ---> this *is* text
 # this is is is text   ---> this *is* text
 # this is text text    ---> this is *text*
 # this is is text text ---> this *is* *text*
-REGEXP_3 = '' 
-REGEXP_3_REPL = ''
+REGEXP_3 = r'\b(\w+)(\s+\1)+\b' 
+REGEXP_3_REPL = r'*\1*'
 
 # one two three ---> two one three
 # dog cat wolf  ---> cat dog wolf
 # goose car rat ---> goose rat car
-REGEXP_4 = '' 
-REGEXP_4_REPL = ''
+REGEXP_4 = r'(\b\w+\b)\s+(\b\w+\b)'
+REGEXP_4_REPL = r'\2 \1'
 
 # cat dog                     ---> cat dog
 # cat dog cat                 ---> cat dog cat
 # dog cat dog cat cat         ---> dog dog
 # dog cat dog rat rat cat cat ---> dog dog rat rat
-REGEXP_5 = '' 
-REGEXP_5_REPL = ''
+REGEXP_5 = r'\b(cat)\b|\b(dog)\b'
+REGEXP_5_REPL = lambda match: '' if match.group(2) else match.group(1)
